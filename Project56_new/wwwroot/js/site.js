5c88864189a30317db16ad0ce745df7b31e34888
fabilies <fabian080@live.nl>
2017-11-30 13:56:19 +0100

    slide

--- Project56_new/wwwroot/js/site.js
+++ Project56_new/wwwroot/js/site.js
@@ -1,1 +1,24 @@
 ﻿// Write your JavaScript code.
+function SliderPagination(page_number , direction) {
+
+    prev = page_number - 1;
+    next = page_number + 1;
+    start = 4;
+
+     if (direction  ==  "next") {
+
+         document.getElementById('col_' + page_number).style.visibility = "hidden";
+         document.getElementById('col_' + (page_number - 1)).style.visibility = "hidden";
+         document.getElementById('col_' + (page_number - 2)).style.visibility = "hidden";
+         document.getElementById('col_' + (page_number - 3)).style.visibility = "hidden";
+
+
+    } else {
+         document.getElementById('col_' + page_number + 4 ).style.visibility = "visible";
+         document.getElementById('col_' + (page_number + 5)).style.visibility = "visible";
+         document.getElementById('col_' + (page_number + 6)).style.visibility = "visible";
+         document.getElementById('col_' + (page_number + 7)).style.visibility = "visible";
+
+        
+    }
+}
