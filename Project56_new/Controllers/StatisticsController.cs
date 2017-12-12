0321ebfffdddd6fac3c47d8337c877d5c0f37d5b
StefanDeWitHR <stefan_de_wit@outlook.com>
2017-12-12 09:15:43 +0100

    Statistics

---
+++ Project56_new/Controllers/StatisticsController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Project56_new.Data;
+using Project56_new.Models;
+namespace Project56_new.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class StatisticsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+
+        public StatisticsController (ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public ActionResult Index(int? page)
+        {
+            var total_orders = _context.OrdMains.Count();
+            var amount_products = _context.Itms.Count();
+            var total_money = _context.OrdHistory.Sum(x => (x.priced_payed * x.qty_bought) );
+
+            ViewBag.total_orders = total_orders;
+            ViewBag.amount_products = amount_products;
+            ViewBag.total_money = total_money;
+            return View();
+        }
+    }
+}
---
+++ Project56_new/Migrations/20171201133934_ordhistory.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Project56_new.Migrations
+{
+    public partial class ordhistory : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "description",
+                table: "OrdHistory");
+
+            migrationBuilder.DropColumn(
+                name: "dt_modified",
+                table: "OrdHistory");
+
+            migrationBuilder.DropColumn(
+                name: "l_show",
+                table: "OrdHistory");
+
+            migrationBuilder.AddColumn<string>(
+                name: "itm_description",
+                table: "OrdHistory",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ord_id",
+                table: "OrdHistory",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ordline_id",
+                table: "OrdHistory",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<float>(
+                name: "priced_payed",
+                table: "OrdHistory",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+
+            migrationBuilder.AddColumn<int>(
+                name: "qty_bought",
+                table: "OrdHistory",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "user_ad",
+                table: "OrdHistory",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "itm_description",
+                table: "OrdHistory");
+
+            migrationBuilder.DropColumn(
+                name: "ord_id",
+                table: "OrdHistory");
+
+            migrationBuilder.DropColumn(
+                name: "ordline_id",
+                table: "OrdHistory");
+
+            migrationBuilder.DropColumn(
+                name: "priced_payed",
+                table: "OrdHistory");
+
+            migrationBuilder.DropColumn(
+                name: "qty_bought",
+                table: "OrdHistory");
+
+            migrationBuilder.DropColumn(
+                name: "user_ad",
+                table: "OrdHistory");
+
+            migrationBuilder.AddColumn<string>(
+                name: "description",
+                table: "OrdHistory",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "dt_modified",
+                table: "OrdHistory",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<int>(
+                name: "l_show",
+                table: "OrdHistory",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
